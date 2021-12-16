d314660927bbf205cb3f3707c83d8ef6c5a6bcd1
SandersLin <coolsanderslin@gmail.com>
2021-12-16 14:30:38 +0800

    entire slider gallery can now be accessed from backend

--- clinicSite/home/models.py
+++ clinicSite/home/models.py
@@ -9,34 +9,53 @@ from modelcluster.fields import ParentalKey
 
 
 class HomePage(Page):
-    def main_image(self):
-        gallery_item = self.slider_images.first()
-        if gallery_item:
-            return gallery_item.image
-        else:
-            return None
+
     def img_slider1(self):
         gallery_item = self.sliderImage1.first()
         if gallery_item:
             return gallery_item.image
         else:
             return None
+    def img_slider2(self):
+        gallery_item = self.sliderImage2.first()
+        if gallery_item:
+            return gallery_item.image
+        else:
+            return None
+    def img_slider3(self):
+        gallery_item = self.sliderImage3.first()
+        if gallery_item:
+            return gallery_item.image
+        else:
+            return None
     content_panels = Page.content_panels + [
-        InlinePanel('slider_images', label="Slider images"),
+
         InlinePanel('sliderImage1', label = "Slider image No.1"),
+        InlinePanel('sliderImage2', label = "Slider image No.2"),
+        InlinePanel('sliderImage3', label = "Slider image No.3"),
+
 
     ]
 
 
-class HomePageGalleryImage(Orderable):
-    page =ParentalKey(HomePage, on_delete=models.CASCADE, related_name='slider_images')
+class HomePageSliderImage1(Orderable):
+    page =ParentalKey(HomePage, on_delete=models.CASCADE, related_name='sliderImage1')
     image = models.ForeignKey('wagtailimages.Image', on_delete=models.CASCADE, related_name='+')
 
     panels = [
         ImageChooserPanel('image'),
     ]
-class HomePageSliderImage1(Orderable):
-    page =ParentalKey(HomePage, on_delete=models.CASCADE, related_name='sliderImage1')
+
+class HomePageSliderImage2(Orderable):
+    page =ParentalKey(HomePage, on_delete=models.CASCADE, related_name='sliderImage2')
+    image = models.ForeignKey('wagtailimages.Image', on_delete=models.CASCADE, related_name='+')
+
+    panels = [
+        ImageChooserPanel('image'),
+    ]
+
+class HomePageSliderImage3(Orderable):
+    page =ParentalKey(HomePage, on_delete=models.CASCADE, related_name='sliderImage3')
     image = models.ForeignKey('wagtailimages.Image', on_delete=models.CASCADE, related_name='+')
 
     panels = [
