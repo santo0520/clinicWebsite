8c163830c62946e4310ac56b7c8dde6c3f1e4e46
SandersLin <coolsanderslin@gmail.com>
2021-12-16 13:17:47 +0800

    got gallery images choose on backend to work

--- clinicSite/home/models.py
+++ clinicSite/home/models.py
@@ -1,7 +1,24 @@
 from django.db import models
 
-from wagtail.core.models import Page
+from wagtail.core.models import Page, Orderable
+from wagtail.admin.edit_handlers import InlinePanel
+from wagtail.images.edit_handlers import ImageChooserPanel
+
+from modelcluster.fields import ParentalKey
+
 
 
 class HomePage(Page):
-    pass
+    content_panels = Page.content_panels + [
+        InlinePanel('gallery_images', label="My gallery images"),
+
+    ]
+
+
+class HomePageGalleryImage(Orderable):
+    page =ParentalKey(HomePage, on_delete=models.CASCADE, related_name='gallery_images')
+    image = models.ForeignKey('wagtailimages.Image', on_delete=models.CASCADE, related_name='+')
+
+    panels = [
+        ImageChooserPanel('image'),
+    ]
