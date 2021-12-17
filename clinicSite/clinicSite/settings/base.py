906e363cfc2b698381e7fa1310859f93a799cafa
SandersLin <coolsanderslin@gmail.com>
2021-12-17 10:27:02 +0800

    fixed collectstatic failed issue by making it forgiving

--- clinicSite/clinicSite/settings/base.py
+++ clinicSite/clinicSite/settings/base.py
@@ -144,7 +144,8 @@ STATICFILES_DIRS = [
 # ManifestStaticFilesStorage is recommended in production, to prevent outdated
 # JavaScript / CSS assets being served from cache (e.g. after a Wagtail upgrade).
 # See https://docs.djangoproject.com/en/3.2/ref/contrib/staticfiles/#manifeststaticfilesstorage
-STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
+STATICFILES_STORAGE = "home.storage.ForgivingManifestStaticFilesStorage"
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
